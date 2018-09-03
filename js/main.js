edec42844b9c5f4a72cd217d3d742572e2e2666c
gitprzemek <przemekorn@gmail.com>
2018-09-03 20:35:39 +0200

    add BEM to cert section and on touch current=60

--- js/main.js
+++ js/main.js
@@ -231,149 +231,42 @@ $(document).ready(function(){
     
 });
 /*SLIDER IN CERT S4ECTION*/
-if (document.documentElement.lang == "en"){
-    var images = [];
-images.push("<img src ='../img/dyploms/Akademia108.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/javascript.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/front-end.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/PSDtoHTML.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/gitvsc.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/angielski.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/MTM-UAS1.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/MTM-UAS2.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/award4.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/award1.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/award2.jpg' class = 'opacImg'>");
-images.push("<img src ='../img/dyploms/award3.jpg' class = 'opacImg'>");
-}else{
-    var images = [];
-images.push("<img src ='img/dyploms/Akademia108.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/javascript.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/front-end.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/PSDtoHTML.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/gitvsc.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/angielski.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/MTM-UAS1.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/MTM-UAS2.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/award4.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/award1.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/award2.jpg' class = 'opacImg'>");
-images.push("<img src ='img/dyploms/award3.jpg' class = 'opacImg'>");
+
+let prevBtn = document.getElementById("prev");
+let nextBtn = document.getElementById("next");
+let listImg = document.getElementsByClassName("list-img__item");
+let listArt = document.getElementsByClassName("list-art__item");
+
+let currentSlide = 1;
+showSlide(currentSlide);
+let currentArticle = 1;
+showArticle(currentArticle);
+
+function showSlide() {
+  let i;
+  if (currentSlide > listImg.length) {currentSlide = 1}    
+  if (currentSlide < 1) {currentSlide = listImg.length}
+  for (i = 0; i < listImg.length; i++) {
+    listImg[i].style.display = "none"; 
+  }
+  listImg[currentSlide-1].style.display = "block";  
 }
-
-
-
-
-var curIndex = 0 ;
-var mainDiv = document.getElementById("my-image-slider");
-var nextBtn = document.getElementById("next");
-var prevBtn = document.getElementById("prev");
-
-mainDiv.innerHTML = images[0];
-
-function getElement() {
-    mainDiv.innerHTML = images[curIndex];
-    /*images.style.opacity = 1;*/
-};
-
-function goNext() {
-    var nextIndex = curIndex + 1;
-    if (nextIndex === images.length) {
-        return 0;
-    } else {
-        return nextIndex;
+function showArticle() {
+    let i;
+    if (currentArticle > listArt.length) {currentArticle = 1}    
+    if (currentArticle < 1) {currentArticle = listArt.length}
+    for (i = 0; i < listArt.length; i++) {
+        listArt[i].style.display = "none";
     }
-};
-
+    listArt[currentArticle-1].style.display = "block";
+  }
 nextBtn.addEventListener("click", function () {
-    curIndex = goNext();
-    getElement();
+    showSlide(currentSlide+= 1);
+    showArticle(currentArticle+= 1);
 });
-
-function goPrev() {
-    var prevIndex = curIndex - 1;
-    if (prevIndex === -1) {
-        return images.length-1;
-    } else {
-        return prevIndex;
-    }
-};
-
 prevBtn.addEventListener("click", function () {
-    curIndex = goPrev();
-    getElement();
-});
-
-/* ARTICLE TO SLIDER*/
-if (document.documentElement.lang == "en"){
-    var articles = [];
-    articles.push("<h3>Programming school</h3><br><p>Bootcamp course Front-end developer at Akademia 108</p>");
-    articles.push("<h3>JavaScript course</h3><br><p>Javascript course from the basics to the advanced level.</p>");
-    articles.push("<h3>Front-end course</h3><br><p>Website creation course with CSS3 and Bootstrap technologies.</p>");
-    articles.push("<h3>PSD to HTML course</h3><br><p>Website creation course based on PSD templates.</p>");
-    articles.push("<h3>Git w Visual Studio Code course</h3><br><p>Git course use VS code editor.</p>");
-    articles.push("<h3>English language course</h3><br><p>Completing the B1 + level English language course.</p>");
-    articles.push("<h3>Certificate MTM-UAS</h3><br><p>Obtaining qualifications to use the MTM-UAS module system.</p>");
-    articles.push("<h3>MTM-1</h3><br><p>Obtaining qualifications for the use of MTM process blocks.</p>");
-    articles.push("<h3>Best project</h3><br><p>I place in the competition for the best project of the year 'Optimization of the logistics window'.</p>");
-    articles.push("<h3>Best project</h3><br><p>Second place in the competition for the best project of the year. 'Application for delivering parts for production'.</p>");
-    articles.push("<h3>Best project</h3><br><p>3rd place in the competition for the best project of the year 'Implementation FIFO system in the warehouse'.</p>");
-    articles.push("<h3>Best project</h3><br><p>Distinction in the competition for the best project of the year 'Rack for parts'.</p>");
-}else{
-    var articles = [];
-    articles.push("<h3>Szkoła programowania</h3><br><p>Kurs bootcamp Front-end developer w szkole Akademia 108</p>");
-    articles.push("<h3>Kurs Javascript</h3><br><p>Kurs Javascript od podstaw do poziomu zawansowanego.</p>");
-    articles.push("<h3>Kurs Front-end</h3><br><p>Kurs tworzenia stron z technologiami CSS3 i Bootstrap.</p>");
-    articles.push("<h3>Kurs PSD do HTML</h3><br><p>Kurs tworzenia stron na podstawie szablonów PSD.</p>");
-    articles.push("<h3>Kurs Git w Visual Studio Code</h3><br><p>Kurs obsługi Git w edytorze VS Code</p>");
-    articles.push("<h3>Kurs języka angielskiego</h3><br><p>Ukończenie kursu języka angielskiego poziom B1+</p>");
-    articles.push("<h3>Certyfikat MTM-UAS</h3><br><p>Uzyskanie kwalifikacji do zastosowania systemu modułów MTM-UAS</p>");
-    articles.push("<h3>MTM-1</h3><br><p>Uzyskanie kwalifikacji do zastosowania bloków procesowych MTM</p>");
-    articles.push("<h3>Najlepszy projekt</h3><br><p>I miejsce w konkursie na najlepszy projekt roku 'Optymalizacja okna logistycznego'</p>");
-    articles.push("<h3>Najlepszy projekt</h3><br><p>II miejsce w konkursie na najlepszy projekt roku 'Aplikacja dotowarowania segmentu GFT20'</p>");
-    articles.push("<h3>Najlepszy projekt</h3><br><p>III miejsce w konkursie na najlepszy projekt roku 'Wprowadzenie FIFO na magazynie'</p>");
-    articles.push("<h3>Najlepszy projekt</h3><br><p>Wyróżnienie w konkursie na najlepszy projekt roku 'Stojak na części'</p>");
-}
-
-
-
-var curIndexArt = 0 ;
-var mainDivArt = document.getElementById("slider-article");
-var nextBtnArt = document.getElementById("next");
-var prevBtnArt = document.getElementById("prev");
-
-mainDivArt.innerHTML = articles[0];
-
-function getElementArt() {
-    mainDivArt.innerHTML = articles[curIndexArt];
-};
-
-function goNextArt() {
-    var nextIndexArt = curIndexArt + 1;
-    if (nextIndexArt === articles.length) {
-        return 0;
-    } else {
-        return nextIndexArt;
-    }
-};
-
-nextBtnArt.addEventListener("click", function () {
-    curIndexArt = goNextArt();
-    getElementArt();
-});
-
-function goPrevArt() {
-    var prevIndexArt = curIndexArt - 1;
-    if (prevIndexArt === -1) {
-        return articles.length-1;
-    } else {
-        return prevIndexArt;
-    }
-};
-
-prevBtnArt.addEventListener("click", function () {
-    curIndexArt = goPrevArt();
-    getElementArt();
+    showSlide(currentSlide+= -1);
+    showArticle(currentArticle+= -1);
 });
 
 // HEADER SECTION CURTAIN ************************************************************************************************************
@@ -450,7 +343,7 @@ if(current < 50){
     // BLUR HEADER AFTER SCROLL BLOCKS
         let aboutSecBlur = (50 - current) / 5;
         let widthWindow= window.innerWidth;
-        if(widthWindow > 758) {
+        if(widthWindow > 1025) {
             aboutSec.style.filter = "blur("+aboutSecBlur+"px)";
         };
 
@@ -492,8 +385,9 @@ $('body').on('wheel', function(event){
 let initialClientY = 0;
 $('body').bind('touchstart', function (event) {
     initialClientY = event.originalEvent.touches[0].clientY;
-    
+    current = 60;
     console.log(initialClientY);
+    updateCurrent(event);
 });
 console.log(initialClientY);
 $('body').bind('touchmove', function (event) {
@@ -502,7 +396,7 @@ $('body').bind('touchmove', function (event) {
     	return;
     }
     
-    current += -(event.originalEvent.touches[0].clientY - initialClientY) / 2;
+    current += -(event.originalEvent.touches[0].clientY - initialClientY) / 1.2;
     // if (current >= 1){
     //     current = 1;
     // }else {
